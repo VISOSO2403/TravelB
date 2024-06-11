c678aaefe7e63479e3ba0d193cbf12e6bfda3b83
Oliver Visoso <ovisosodev@gmail.com>
2024-06-11 17:23:28 -0600

    add stack navigation and signup screen

--- src/screens/auth/SignInScreen.tsx
+++ src/screens/auth/SignInScreen.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {
   Keyboard,
   Pressable,
@@ -5,10 +6,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
 import {
   InputComponent,
   LogoComponent,
+  PrimaryButton,
   ScreenContainer,
   SectionContainer,
   TextComponent,
@@ -21,10 +24,12 @@ const SignInScreen = () => {
   const [newPassword, setNewPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
 
+  const navigation = useNavigation();
+
   return (
     <ScreenContainer>
       <Pressable style={{flex: 1}} onPress={Keyboard.dismiss}>
-        <SafeAreaView style={{}} />
+        <SafeAreaView />
         <LogoComponent />
 
         <SectionContainer>
@@ -34,7 +39,7 @@ const SignInScreen = () => {
 
         <SectionContainer>
           <InputComponent
-            onChangeText={val => setEmail(val)}
+            // onChangeText={val => setEmail(val)}
             placeholder="Correo"
             keyboardType="email-address"
           />
@@ -54,19 +59,12 @@ const SignInScreen = () => {
               />
             </TouchableOpacity>
           </RowContainer>
-
-          <TouchableOpacity
-            activeOpacity={0.8}
-            onPress={() => {}}
-            style={[
-              stylesButton.primaryButton,
-              stylesButton.shadow,
-              {backgroundColor: '#f3f3f6'},
-            ]}>
-            <TextComponent text="INGRESAR" font="bold" />
-          </TouchableOpacity>
+          <PrimaryButton onPress={() => {}}>
+            <TextComponent text="INGRESAR" color="white" font="bold" />
+          </PrimaryButton>
         </SectionContainer>
 
+        {/* Seccion para colocar inicio de sesión con Google y/o apple */}
         <SectionContainer>
           <RowContainer styles={{paddingVertical: 20}} isCenter>
             <View style={{flex: 1, borderWidth: 0.4}} />
@@ -85,7 +83,7 @@ const SignInScreen = () => {
           <TouchableOpacity
             activeOpacity={0.8}
             style={{marginLeft: 5}}
-            onPress={() => {}}>
+            onPress={() => navigation.navigate('SignUp')}>
             <TextComponent text="Regístrate" color="#491eb4" />
           </TouchableOpacity>
         </RowContainer>
